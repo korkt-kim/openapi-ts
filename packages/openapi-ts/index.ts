aa8e5ce3fb3d95224f82c00628fd563676066f3c
korkt-kim <gnscott6@naver.com>
2025-07-25 18:38:55 +0900

    feat: 기본명령 option 정의

--- packages/openapi-ts/index.ts
+++ packages/openapi-ts/index.ts
@@ -1,28 +1,69 @@
 #!/usr/bin/env node
- 
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
 
-const args = yargs(hideBin(process.argv))
+import yargs, { Argv } from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import { generateApi } from './src/command'
+import { OpenApiTsProps } from './src/types'
+
+const args = (yargs(hideBin(process.argv)) as Argv<OpenApiTsProps>)
   .parserConfiguration({
     'camel-case-expansion': false,
     'greedy-arrays': false,
     'short-option-groups': false,
     'unknown-options-as-args': true,
   })
-  .command('[input]','Run the opeanapi-ts command', (yargs) => 
-    // @TODO: 미정
-     yargs.option('not implemented', {
-      description: 'Not Implemented',
-      type: 'string',
+  .command('$0', 'Run the opeanapi-ts command', yargs =>
+    yargs.options({
+      path: {
+        alias: 'p',
+        description: 'path/url to swagger scheme',
+        type: 'string',
+        demandOption: true,
+      },
+      output: {
+        alias: 'o',
+        description: 'output path of typescript api file',
+        type: 'string',
+        default: './',
+      },
+      preserve: {
+        description: 'preserve the name of the already exported api',
+        type: 'boolean',
+        default: false,
+      },
+      'extract-query-params': {
+        description:
+          'extract request params to data contract (Also combine path params and query params into one object) (default: false)',
+        type: 'boolean',
+        default: false,
+      },
+      'extract-request-body': {
+        description:
+          'extract request body type to data contract (default: false)',
+        type: 'boolean',
+        default: false,
+      },
+      'extract-response-body': {
+        description:
+          'extract response body type to data contract (default: false)',
+        type: 'boolean',
+        default: false,
+      },
+      'patch-type': {
+        description: 'fix the type of extracted type (default: [])',
+        type: 'array',
+        default: [],
+      },
+      module: {
+        description: 'generate separated as module (default: none)',
+        type: 'string',
+        default: 'none',
+      },
     })
   )
-  .demandCommand(1)
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   .version(require('./package.json').version)
-  .showHelpOnFail(true).parse()
+  .showHelpOnFail(true)
+  .parseSync()
 
-//
-// args(openapiTsCommand)
-
-console.log(args)
+generateApi(args)
